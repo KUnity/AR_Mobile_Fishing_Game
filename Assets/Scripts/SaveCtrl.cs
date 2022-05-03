06196100c8fcf72f198598a600e60cde7ba004c0
Cheonnyang <57510872+gus6615@users.noreply.github.com>
2022-05-03 17:05:56 +0900

    feat: Backend 추가

--- Assets/Scripts/SaveCtrl.cs
+++ Assets/Scripts/SaveCtrl.cs
@@ -2,16 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using BackEnd;
+using LitJson;
+using System.Text;
 
 /// <summary> User Data 저장 Class </summary>
 public class UserData
 {
-    /// <summary> User Identifier </summary>
-    public string owner_inDate;
-    /// <summary> User Nickname </summary>
-    public string nickName;
+    /// <summary> User Data InDate </summary>
+    public string data_inDate;
+    /// <summary> User ID </summary>
+    public string ID;
     /// <summary> User가 가진 재화 </summary>
-    public int gold;
+    public long gold;
     /// <summary> User가 잡은 물고기의 갯수 </summary>
     public int[] fishNums;
     /// <summary> User가 소유한 미끼의 갯수 </summary>
@@ -23,13 +27,20 @@ public class UserData
     /// <summary> User의 현재 rank </summary>
     [Obsolete] // 2차 구현에서 랭크 시스템 도입 시 사용 예정
     public int rank;
+
+    public UserData()
+    {
+        fishNums = new int[5];
+        fishBaits = new int[5];
+        hasFishingRod = new bool[5];
+    }
 };
 
 public class SaveCtrl : MonoBehaviour
 {
     public static SaveCtrl instance = null;
-    public UserData myData;
 
+    public UserData myData;
     [Obsolete] // 2차 구현에서 로그인 & 회원가입 기능 도입 시 사용 예정
     public List<UserData> userDatas = new List<UserData>();
     public List<FishingRob> fishingRobs = new List<FishingRob>();
@@ -53,7 +64,19 @@ public class SaveCtrl : MonoBehaviour
         if (null == instance)
         {
             instance = this;
+            myData = new UserData();
             DontDestroyOnLoad(this.gameObject);
+
+            var bro = Backend.Initialize(true);
+            if (bro.IsSuccess())
+            {
+                //Backend.BMember.DeleteGuestInfo();
+                Login();
+            }
+            else
+            {
+                Debug.Log("서버 초기화 실패 : " + bro.GetErrorCode());
+            }
 
             // 낚시대 데이터 생성
             for (int i = 0; i < FishingRob.fishingRobNum; i++)
@@ -69,26 +92,116 @@ public class SaveCtrl : MonoBehaviour
     }
 
     /// <summary>
+    /// 로그인 시도 함수
+    /// </summary>
+    public void Login()
+    {
+        if(Backend.BMember.GetGuestID().Equals(""))
+        {
+            // ID 없음 -> 데이터 초기화
+            Backend.BMember.GuestLogin();
+            InsertData();
+            Debug.Log("회원가입 및 데이터 초기화");
+        }
+        else
+        {
+            string id = Backend.BMember.GetGuestID();
+            BackendReturnObject bro = Backend.BMember.GuestLogin();
+
+            Debug.Log("로컬 기기에 저장된 아이디 :" + id);
+            if (bro.IsSuccess())
+            {
+                LoadData();
+            }
+            else
+            {
+                Debug.Log("\n로그인 실패 : " + bro.GetMessage());
+            }
+        }
+    }
+
+
+    /// <summary>
     /// User Data를 서버에 저장합니다.
     /// </summary>
-    [Obsolete] // 2차 구현에서 사용 예정
     public void SaveData()
     {
-        
+        if (!Backend.IsInitialized) return;
+        string errorCode = null;
+
+        Param param = new Param();
+        SettingParam(param);
+
+        errorCode = Backend.GameData.Update("userData", myData.data_inDate, param).GetErrorCode();
+        Debug.Log("UserData 서버 저장 <error> = " + errorCode);
     }
 
     /// <summary>
     /// 서버로부터 User Data를 불러옵니다.
     /// </summary>
-    [Obsolete] // 2차 구현에서 사용 예정
     public void LoadData()
     {
-
+        if (!Backend.IsInitialized) return;
+        BackendReturnObject BRO = Backend.GameData.GetMyData("userData", new Where());
+        if (BRO.IsSuccess())
+        {
+            JsonData json2 = BRO.GetReturnValuetoJSON()["rows"][0];
+            DataParsing(json2);
+        }
+        else
+        {
+            Debug.Log("서버 공통 에러 발생: " + BRO.GetMessage());
+        }
     }
 
-    private void A()
+    /// <summary>
+    /// 데이터 테이블 추가 함수
+    /// </summary>
+    private void InsertData()
     {
-        Debug.Log("첫번째 낚시대 가격 : " + SaveCtrl.instance.fishingRobs[0].gold + " 입니다.");
-        Debug.Log("두번째 미끼 가격 : " + SaveCtrl.instance.baits[1].gold + " 입니다.");
+        Param param = new Param();
+        SettingParam(param);
+
+        BackendReturnObject BRO = Backend.GameData.Insert("userData", param);
+        myData.data_inDate = BRO.GetInDate();
+        myData.ID = Backend.BMember.GetGuestID();
+
+        string error = BRO.GetErrorCode();
+        Debug.Log("public param save <error> : " + error);
+
+        SaveData();
+    }
+
+    /// <summary>
+    /// param을 설정하는 함수
+    /// </summary>
+    /// <param name="param">데이터 전달 Parameter</param>
+    private void SettingParam(Param param)
+    {
+        param.Add("data_inDate", myData.data_inDate);
+        param.Add("ID", myData.ID);
+        param.Add("gold", myData.gold);
+        param.Add("fishNums", myData.fishNums);
+        param.Add("fishBaits", myData.fishBaits);
+        param.Add("hasFishingRod", myData.hasFishingRod);
+        param.Add("equipFishingRod", myData.equipFishingRod);
+    }
+
+    /// <summary>
+    /// 서버로부터 얻은 데이터를 클라이언트에 저장
+    /// </summary>
+    /// <param name="json">서버 데이터 JsonData</param>
+    private void DataParsing(JsonData json)
+    {
+        myData.data_inDate = json["data_inDate"][0].ToString();
+        myData.ID = json["ID"][0].ToString();
+        myData.gold = long.Parse(json["gold"][0].ToString());
+        for (int i = 0; i < json["fishNums"]["L"].Count; i++)
+            myData.fishNums[i] = int.Parse(json["fishNums"]["L"][i][0].ToString());
+        for (int i = 0; i < json["fishBaits"]["L"].Count; i++)
+            myData.fishBaits[i] = int.Parse(json["fishBaits"]["L"][i][0].ToString());
+        for (int i = 0; i < json["hasFishingRod"]["L"].Count; i++)
+            myData.hasFishingRod[i] = bool.Parse(json["hasFishingRod"]["L"][i][0].ToString());
+        myData.equipFishingRod = int.Parse(json["equipFishingRod"][0].ToString());
     }
 }
---
+++ Assets/TheBackend/Toolkit/SendQueueMgr.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+using BackEnd;
+
+// SendQueue를 손쉽게 사용하기 위한 SendQueueMgr 클래스 입니다.
+// 해당 스크립트를 유니티 객체에 스크롤 해서 적용해주세요.
+// SendQueue에 대한 자세한 설명은 아래 개발자 문서를 참고해주세요.
+// https://developer.thebackend.io/unity3d/guide/Async/SendQueue/
+
+public class SendQueueMgr : MonoBehaviour
+{
+    void Awake()
+    {
+        // 씬이 바뀌어도 계속 SendQueue를 사용하기 위해서
+        // 아래 DontDestroyOnLoad 함수를 통해 SendQueue 객체를 파괴되지 않는 오브젝트로 만듭니다.
+        // 자세한 설명은 아래 유니티 개발자문서를 참고해주세요.
+        // https://docs.unity3d.com/kr/current/ScriptReference/Object.DontDestroyOnLoad.html
+        DontDestroyOnLoad(this);
+
+        // 만약 SendQueue가 초기화 되지 않았다면 초기화 수행
+        if (SendQueue.IsInitialize == false)
+        {
+            // SendQueue는 시작과 동시에 초기화가 수행됩니다.
+            // 디버그 로그 활성화, 예외 이벤트 핸들러 등록
+            SendQueue.StartSendQueue(true, ExceptionEvent);
+        }
+    }
+
+    void Update()
+    {
+        // SendQueue가 초기화 되었을 때만 Poll 함수를 호출
+        if (SendQueue.IsInitialize)
+        {
+            // SendQueue를 정상적으로 사용하기 위해서는 
+            // 아래 Poll 함수가 반드시 정기적으로 호출되어야 합니다.
+            BackEnd.SendQueue.Poll();
+        }
+    }
+
+    // SendQueue 내부에서 예외가 발생했을 경우  
+    // 아래 이벤트 핸들러를 통해 예외 이벤트가 전달됩니다.
+    void ExceptionEvent(Exception e)
+    {
+        Debug.Log(e.ToString());
+    }
+
+
+    void OnApplicationPause(bool isPause)
+    {
+        if (isPause == false)
+        {
+            // 어플리케이션이 재실행 되었을 때 SendQueue를 재실행 시킴
+            SendQueue.ResumeSendQueue();
+        }
+        else
+        {
+            // 어플리케이션이 정지되었을 때 SendQueue를 일시 정지 시킴 
+            SendQueue.PauseSendQueue();
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        // 어플리케이션이 종료되었을 때 SendQueue를 정지 시킴
+        BackEnd.SendQueue.StopSendQueue();
+    }
+}
