bdba52ab559ea8f13a0de7167f4e76fdcf5214aa
c-jeongyyun <99jenny0614@naver.com>
2022-05-05 02:53:50 +0900

    feat: 인벤토리 스크롤 UI 작성

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] public GameObject itemInfo;
+        // Start is called before the first frame update
     void Start()
     {
         // 데이터에서 보유 아이템 불러오기 (배열로) 
         // UI에 맞춰 아이템  배치
 
+
+    }
+
+    public void OnSlotClick(){
 
     }
 
