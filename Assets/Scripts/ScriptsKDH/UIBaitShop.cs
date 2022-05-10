d8c1213e65980cf5c38f6a67c73e00274b7a1bd2
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2022-05-10 22:55:02 +0900

    Shop UI 수정 #4
    -이미지 적용 변경
    -버튼 click 횟수 제한
    -DB연동 완료
    -물품에 따른 이미지 출력

--- Assets/Scripts/ScriptsKDH/UIBaitShop.cs
+++ Assets/Scripts/ScriptsKDH/UIBaitShop.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D;
 using UnityEngine.UI;
+using UnityEngine.Sprites;
 using TMPro;
 
 public class UIBaitShop: MonoBehaviour
@@ -11,47 +12,18 @@ public class UIBaitShop: MonoBehaviour
     public GameObject contents;
     public GameObject uiListItemPrefab;
     public TMP_Text priceText;
-    public float iconWidth = 180;
-    public float iconHeight = 180;
-
-    //string[] itemNames = {
-    //    "Shrimp",
-    //    "Warm",
-    //    "Dacaied fish",
-    //    "Cabire",
-    //    "Squid",
-    //    "Pearl"
-    //};
-
-    //string[] itemDescription = {
-    //    "It's a warm",
-    //    "It's a Decaied fish",
-    //    "It's a shrimps",
-    //    "It's a Squid",
-    //    "It's a Cabire",
-    //    "It's a Pearl"
-    //};
-
-    //int[] itemPrices = {
-    //    1,
-    //    5,
-    //    10,
-    //    25,
-    //    50,
-    //    100
-    //};
 
     void Start()
     {
         for (int i = 0; i < Bait.BaitNum; i++)
         {
-            var itemIcon = string.Format("R2_{0}", i);
+            var itemIcon = string.Format("HPBar_{0}", i);
             string iName = SaveCtrl.instance.baits[i].name;
             long iPrice = SaveCtrl.instance.baits[i].gold;
             string iDescription = SaveCtrl.instance.baits[i].desc;
-            var sp = atlas.GetSprite(itemIcon);
-            var go = Instantiate<GameObject>(this.uiListItemPrefab, contents.transform);
-            var uiListItem = go.GetComponent<UIListItem>();
+            Sprite sp = atlas.GetSprite(itemIcon);
+            GameObject go = Instantiate<GameObject>(this.uiListItemPrefab, contents.transform);
+            UIListItem uiListItem = go.GetComponent<UIListItem>();
             uiListItem.btn.onClick.AddListener(() =>
             {
                 if (SaveCtrl.instance.myData.gold < iPrice)
--- Assets/Scripts/ScriptsKDH/UIFRShop.cs
+++ Assets/Scripts/ScriptsKDH/UIFRShop.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class UIFRShop : MonoBehaviour
@@ -12,48 +13,14 @@ public class UIFRShop : MonoBehaviour
     public GameObject uiListItemPrefab;
     public TMP_Text priceText;
     public Sprite soldout;
-    public float iconWidth = 180;
-    public float iconHeight = 180;
-
-    //string[] itemNames = {
-    //    "Shabby fishing rod",
-    //    "Bamboo fishing rod",
-    //    "Steal fishing rod",
-    //    "bronze fishing rod",
-    //    "Silver fishing rod",
-    //    "Gold fishing rod",
-    //    "Diamond fishing rod",
-    //    "Carbon  fishing rod",
-    //};
-
-    //string[] itemDescription = {
-    //    "It's a fishing rod thrown away by the shop uncle.",
-    //    "It's a fishing rod made of Bamboo",
-    //    "It's a fishing rod made of Steal",
-    //    "It's a fishing rod made of bronze",
-    //    "It's a fishing rod made of Silver",
-    //    "It's a fishing rod made of Gold",
-    //    "It's a fishing rod made of Diamond",
-    //    "It's a fishing rod made of Carbon",
-    //};
-
-    //int[] itemPrices = {
-    //    1,
-    //    2,
-    //    3,
-    //    4,
-    //    5,
-    //    6,
-    //    7,
-    //    8,
-    //};
 
     void Start()
     {
+        Debug.Log("Shop Start() Starts");
         for (int i = 0; i < FishingRob.fishingRobNum; i++)
         {
-            var itemIcon = string.Format("HPBar_{0}", i+19);
-
+            int temp = i;
+            var itemIcon = string.Format("HPBar_{0}", i + 19);
             string iName = SaveCtrl.instance.fishingRobs[i].name;
             long iPrice = SaveCtrl.instance.fishingRobs[i].gold;
             string iDescription = SaveCtrl.instance.fishingRobs[i].desc;
@@ -62,55 +29,29 @@ public class UIFRShop : MonoBehaviour
             var go = Instantiate<GameObject>(this.uiListItemPrefab, contents.transform);
             var uiListItem = go.GetComponent<UIListItem>();
             uiListItem.Init(sp, iName, iDescription, iPrice);
-
-            SaveCtrl.instance.LoadData();
-
-            if (SaveCtrl.instance.myData.hasFishingRod[i] == false){
-                uiListItem.btn.interactable = false;
-                SaveCtrl.instance.SaveData();
-                uiListItem.changeImage(soldout);
-            }
-
             uiListItem.btn.onClick.AddListener(() =>
             {
-                if (SaveCtrl.instance.myData.gold >= iPrice){
+                if (SaveCtrl.instance.myData.gold < iPrice){
                     Debug.Log("보유 골드가 부족합니다.");
-
                 }
                 else{
-                    SaveCtrl.instance.LoadData();
+                    Debug.Log(temp + " temp");
                     SaveCtrl.instance.myData.gold -= iPrice;
-                    switch (i)
+                    if(SaveCtrl.instance.myData.hasFishingRod[temp] == false)
                     {
-                        case 0:
-                            SaveCtrl.instance.myData.hasFishingRod[0] = true;
-                            //uiListItem.btn.interactable = !SaveCtrl.instance.myData.hasFishingRod[0];
-                            
-                            break;
-                        case 1:
-                            SaveCtrl.instance.myData.hasFishingRod[1] = true;
-                            //uiListItem.btn.interactable = !SaveCtrl.instance.myData.hasFishingRod[1];
-                            break;
-                        case 2:
-                            SaveCtrl.instance.myData.hasFishingRod[2] = true;
-                            //uiListItem.btn.interactable = !SaveCtrl.instance.myData.hasFishingRod[2];
-                            break;
-                        case 3:
-                            SaveCtrl.instance.myData.hasFishingRod[3] = true;
-                            //uiListItem.btn.interactable = !SaveCtrl.instance.myData.hasFishingRod[3];
-                            break;
-                        case 4:
-                            SaveCtrl.instance.myData.hasFishingRod[4] = true;
-                            //uiListItem.btn.interactable = !SaveCtrl.instance.myData.hasFishingRod[4];
-                            break;
+                        SaveCtrl.instance.myData.hasFishingRod[temp] = true;
                     }
                     uiListItem.btn.interactable = false;
                     uiListItem.changeImage(soldout);
                     priceText.text = SaveCtrl.instance.myData.gold.ToString() + " G";
-                    SaveCtrl.instance.SaveData();
-
                 }
+                SaveCtrl.instance.SaveData();
             });
+            if (SaveCtrl.instance.myData.hasFishingRod[i] == true)
+            {
+                uiListItem.btn.interactable = false;
+                uiListItem.changeImage(soldout);
+            }
         }
         priceText.text = SaveCtrl.instance.myData.gold.ToString() + " G";
     }
