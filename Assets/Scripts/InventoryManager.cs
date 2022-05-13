1c14307845cde8e482533b561cb1262c9e2816da
c-jeongyyun <99jenny0614@naver.com>
2022-05-14 00:09:50 +0900

    feat : 아이템 슬롯 이벤트추가

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -19,39 +19,71 @@ public class InventoryManager : MonoBehaviour
     {
         goldNum.text = SaveCtrl.instance.myData.gold.ToString();
         GameObject slotPrefab = Resources.Load("Prefabs/Slot") as GameObject;
-        //Debug.Log(SaveCtrl.instance.myData.fishBaits);
+
         // 데이터에서 보유 아이템 불러오기 (배열로) 
+       
+        // UI에 맞춰 아이템  배치 + Slot 동적 생성 
+        int havingFishingRodCnt=0;
+        bool[] hasFishingRod = SaveCtrl.instance.myData.hasFishingRod;
+        int size = hasFishingRod.Length;
+        for(int i =0;i<size;i++){
+            if(hasFishingRod[i])
+                havingFishingRodCnt++;
+        }
+
         // 사용자가 기존에 선택한 아이템으로 oldSelectedSlot 초기화 
         // oleSelectedSlot =
-        // UI에 맞춰 아이템  배치 + Slot 동적 생성 
-        int size = 5;
-        Debug.Log(slotParent);
-        for(int i =0;i<size;i++){
+
+        for(int i =havingFishingRodCnt;i>-1;i--){
             GameObject slot = Instantiate(slotPrefab);
             slot.name="Slot"+i; 
             slot.transform.SetParent(slotParent.transform); 
+            
+            if(i==0){   
+                oldSelectedSlot=slot;
+                oldSelectedSlot.transform.GetChild(0).gameObject.SetActive(true);
+                SetRodItemInfo(oldSelectedSlot,SaveCtrl.instance.myData.equipFishingRod);
+                oldSelectedSlot.GetComponent<Outline>().enabled = true;
+            }
         }
 
 
 
     }
 
+    public void SetRodItemInfo(GameObject slot,int itemCode){
+        bool[] hasFishingRod = SaveCtrl.instance.myData.hasFishingRod;
+        (itemInfo.transform.Find("ItemName").GetComponent<TextMeshProUGUI>()).text = FishingRob.robNames[itemCode];
+        (itemInfo.transform.Find("Probability").GetComponent<TextMeshProUGUI>()).text = "Prob : "+ FishingRob.probalility_datas[itemCode].ToString();
+        (itemInfo.transform.Find("Power").GetComponent<TextMeshProUGUI>()).text =  "Power : "+FishingRob.power_datas[itemCode].ToString();
+        (itemInfo.transform.Find("Description").GetComponent<TextMeshProUGUI>()).text = FishingRob.robDesc[itemCode];
+    }
+
     public void OnSlotClick(){
+        if(newSelectedSlot!= null){
+            oldSelectedSlot = newSelectedSlot;
+        }
         newSelectedSlot = EventSystem.current.currentSelectedGameObject;
+        Debug.Log(newSelectedSlot.name);
         // name, probablity, power, description 가져와서 item info창에 넣어주기 
+       showOutline();
 
     }
 
+
+    // 그냥 클릭만 하는 경우
+    public void showOutline(){
+        // 테두리 보이게 
+        newSelectedSlot.GetComponent<Outline>().enabled = true;
+        oldSelectedSlot.GetComponent<Outline>().enabled = false;
+    }
+
+    // 실제 착용하는 경우
     public void onUseItemClick(){
         // 체크 표시
         newSelectedSlot.transform.GetChild(0).gameObject.SetActive(true);
         oldSelectedSlot.transform.GetChild(0).gameObject.SetActive(false);
     }
-    // public void UnuseItem(){
-
-    //     // 기본적으로 선택되는 아이템을 지정할지 
-    //     // 
-    // }
-
+ 
 
 }
