9b8a3aaf7e5634757506c889c7f2db4bcb89b8bf
Cheonnyang <57510872+gus6615@users.noreply.github.com>
2022-05-14 11:37:24 +0900

    feat: MotionBlur + SaveCtrl 추가

---
+++ Assets/Scripts/MotionBlur.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MotionBlur : MonoBehaviour
+{
+    private static float throwForce = 5.5f;
+    private static float chamjilForce = 3.0f;
+
+    /// <summary>
+    /// 낚시대 던지는 모션을 감지하는 함수
+    /// </summary>
+    /// <returns></returns>
+    public static bool CheckThrow()
+    {
+        return Input.acceleration.magnitude > throwForce;
+    }
+
+    /// <summary>
+    /// Hooking을 감지하는 함수
+    /// </summary>
+    /// <returns></returns>
+    public static bool CheckChamjil()
+    {
+        return Input.acceleration.magnitude > chamjilForce;
+    }
+}
