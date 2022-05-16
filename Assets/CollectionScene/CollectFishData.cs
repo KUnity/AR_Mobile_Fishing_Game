080861ff6a41dec0d528af65c348e83992eba493
smk8753 <104252195+smk8753@users.noreply.github.com>
2022-05-16 15:25:17 +0900

    도감완성, 씬연결 다시

--- Assets/CollectionScene/CollectFishData.cs
+++ Assets/CollectionScene/CollectFishData.cs
@@ -5,19 +5,14 @@ using UnityEngine.UI;
 
 public class CollectFishData : MonoBehaviour
 {
-    [SerializeField] private Text fishNumText;
-    [SerializeField] private Text fishNameText;
     [SerializeField] private Text powerUpText;
     [SerializeField] private Text probUpText;
-    public int fishNum;
-    public string fishName;
+
     public float powerUp;
     public float probUp;
     // Start is called before the first frame update
     void Start()
     {
-        fishNumText.text = string.Format("NO. {0}", fishNum);
-        fishNameText.text = fishName;
         powerUpText.text = string.Format("파워 업 : +{0:F1}", powerUp);
         probUpText.text = string.Format("확률 업 : +{0:F1}%", probUp);
     }
