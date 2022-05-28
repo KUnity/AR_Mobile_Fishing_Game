09cc676470b02c9328a8d2e865de5a3acdf589fe
Cheonnyang <57510872+gus6615@users.noreply.github.com>
2022-05-28 23:43:25 +0900

    feat | Ranking System

---
+++ Assets/Scripts/UIBox.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBox : MonoBehaviour
+{
+    public Image[] images;
+    public Text[] texts;
+    public Button button;
+}
